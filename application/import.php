178177359bd444e9473c92998be071048342810f
JonasHaener <jon.haener@gmail.com>
2014-03-23 16:48:58 +0800

    drag and drop

---
+++ application/import.php
@@ -0,0 +1,35 @@
+<?php
+//session_start();
+
+// get requests
+require_once "includes/class_db_stats.inc.php";
+// config file
+require_once "includes/config.inc.php";
+require_once "includes/config_user.inc.php";
+
+
+http_response_code(200);
+echo "File received";
+/*
+## configs
+$server			= 	$_SERVER;
+$request  		=	$_REQUEST;
+$request_method =   $server['REQUEST_METHOD'];
+$data 			= 	file_get_contents('php://input');
+
+if($request_method !== "GET") {
+	throw new Exception("Wrong request type", 1);
+	exit();
+}
+	
+## make project calls
+$stats = new Stats();
+// set user (config file)
+$stats->set_user($user);
+// sets GET, POST, UPDATE or DELETE
+$stats->set_request($request, $server, $data);
+// connect to database
+$stats->connect_db( $config_host, $config_db_user, $config_db_pwd, $config_database );
+// route request to appropriate handler
+$stats->route_request();
+*/
--- public/script/statistics/views/v_stats_import.js
+++ public/script/statistics/views/v_stats_import.js
@@ -17,7 +17,10 @@ define(function(require){
 		template: _.template( templ ),
 
 		events: {
-		
+			//"dragenter .js_dropbox" : "addDropBoxMarking",
+			"drop .js_dropbox"         : "uploadFile",
+			"dragover .js_drop_here"   : "addDropBoxMarking",
+			"dragleave .js_drop_here"  : "addDropBoxMarking"
 		},
 
 		collections: {
@@ -32,8 +35,13 @@ define(function(require){
 		},
 
 		CSS: {
-
+			hide: "egg-is-hidden",
+			dragOver: "egg-is-drop-over"
 		},
+
+		hooks: {},
+
+		timer: null,
 
 		initialize: function() {
 			
@@ -62,15 +70,145 @@ define(function(require){
 			
 		render: function() {
 			this.$el.html( this.template() );
-			//this.assignHooks();
-			//this.addListeners();
+			this.assignHooks();
+			this.addListeners();
 			return this;
 		},
 
 		assignHooks: function()
 		{	
+			this.hooks.dropbox = this.$(".js_dropbox");
+			this.hooks.dropHere = this.$(".js_drop_here");
+			this.hooks.uploadProgress = this.$(".js_upload_progress");
+		},
+
+		addDropBoxMarking: function(e)
+		{
+			var _this = this;
+			// create temp object
+			e.preventDefault();
+			e.stopPropagation();
+
+			if(e.type === "dragover") {
+				// if timer runs exit
+				if(this.timer) { return; }
+				this.hooks.dropHere.addClass(_this.CSS.dragOver);
+				this.timer = setTimeout(function() {
+			 		_this.timer = null;
+				}, 1000);
+			}
+			
+			if(e.type === "dragleave") {
+			 	this.hooks.dropHere.removeClass(_this.CSS.dragOver);
+			 	this.timer = null;
+			}
+		},
+
+		updatePerc: function(value, remove) {
+			var perc = value + "%",
+				_this = this;
+			
+			this.hooks.uploadProgress
+				.parent().removeClass(this.CSS.hide)
+				.end()
+				.css("width", perc)
+				.text(perc);
+			
+			if(remove === true) {
+				setTimeout(function() {
+					_this.hooks.uploadProgress	
+						.parent().addClass(_this.CSS.hide)
+						.end()
+						.css("width", "5%")
+						.text("0%");
+				}, 1000);
+			}
+		},
+
+		updateFileSize: function(file) {
+			// 1MB = bytes / pwr(1024, 2)
+			// 1KB = 1024 bytes
+			var conv_MB = Math.pow(1024, 2),
+				conv_KB = 1024,
+				MBytes = (file.size/conv_KB).toFixed(2);
+
+			this.hooks.dropHere.text(MBytes+"KB");
 
 		},
+
+		uploadFile: function(e)
+		{
+			e.preventDefault();
+			e.stopPropagation();
+
+			var _this = this,
+				fileOne = e.originalEvent.dataTransfer.files[0],
+				fileName = fileOne.name,
+				placeholder = this.hooks.dropHere.text();
+
+			this.updateFileSize(fileOne);
+
+			// remove drag over class as not removed when upload starts		
+			this.hooks.dropHere.removeClass(this.CSS.dragOver);
+
+			// add progress bar	
+			this.router.inprogress(true);
+
+			$.when($.ajax({
+				
+				url: "/textcrunch33/application/import.php",
+
+				type: "POST",
+				
+				data: { file: fileOne },
+
+				// do not stringify data before
+				// sending
+				processData: false,
+
+				xhr: function() {
+					var xhr = new window.XMLHttpRequest();
+
+					xhr.upload.addEventListener("progress", function(e) {
+						
+						if(e.lengthComputable) {
+							var percCompl = (e.loaded / e.total)*100;
+							_this.updatePerc(percCompl, false);
+						}
+					}, false);
+
+					xhr.upload.addEventListener("load", function(e) {
+						// 100% loaded
+						_this.updatePerc(100, true);
+					}, false);
+					
+					return xhr;
+				}
+				
+			})).then(
+				// success
+				function(data, status, xhr) {						
+					// notify user of saved 
+					_this.router.actionSuccess();
+					// remove progress bar
+					_this.router.inprogress(false);
+					// reset default text
+					_this.hooks.dropHere.text(placeholder);
+				},
+				// fail
+				function(xhr, status, error) {
+					_this.router.actionError();
+					// remove progress bar
+					_this.router.inprogress(false);
+					// load failed
+					_this.updatePerc(0, true);
+					// reset default text
+					_this.hooks.dropHere.text(placeholder);					
+				}
+			);	
+			
+			console.log("File received");
+		}
 
 	});
 
